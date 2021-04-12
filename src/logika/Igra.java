b3a8e3439766c82601ba81838ac958be0c3ed277
zstrgar <zstrgar@gmail.com>
2021-04-12 11:15:33 +0200

    metode v igra

--- src/logika/Igra.java
+++ src/logika/Igra.java
@@ -9,31 +9,33 @@ import splosno.Koordinati;
 public class Igra {
 	
 	//Velikost igralne plošèe
-	public static final int N = 15;
+	public static final int N = 15;  //final, da je N fiksiran
 	
-	//Igralno polje
+	//Igralna plosca, ki sestoji iz polj(prazno, crno, belo)
 	private Polje[][] plosca;
 	
-	//Igralec, ki je trenutno na potezi
+	//Igralec (crn, bel), ki je trenutno na potezi
 	public Igralec naPotezi;
 	
 /**
-* Nova igra, v zaèetni poziciji je prazna in na potezi je CRN.
+* Nova igra, na zaèetku je prazna in na potezi je CRN.
 */
-	
-	public Igra() {
+	public Igra() {  				//konstruktor
+		
 		plosca = new Polje[N][N];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				plosca[i][j] = Polje.PRAZNO;
 			}
 		}
+		
 		naPotezi = Igralec.CRN;
 	}
 	
-/**
-* seznam možnih potez
-*/
+	/**
+	 * metoda moznePoteze() vrne seznam List<Koordinati> možnih potez 
+	 * @return moznePoteze
+	 */
 	public List<Koordinati> moznePoteze() {
 		LinkedList<Koordinati> moznePoteze = new LinkedList<Koordinati>();
 		for (int i = 0; i < N; i++) {
@@ -46,10 +48,38 @@ public class Igra {
 		return moznePoteze;
 	}
 	
-/** 
- * TODO: 
- * public boolean odigraj(Koordinati koordinati)
- */
+
+	/**
+	 * metoda veljavnostPoteze preveri, èe je poteza veljavna
+	 * preveri, èe je polje prazno in èe sta koordinati X in Y med 0 in 15
+	 * @param poteza
+	 * @return true èe je veljavna, false, èe ni
+	 */
+	public boolean veljavnostPoteze(Koordinati poteza) {
+		if (plosca[poteza.getX()][poteza.getY()] != Polje.PRAZNO) return false;
+		if (poteza.getX() < 0 || poteza.getX() > N) return false;
+		if (poteza.getY() < 0 || poteza.getY() > N) return false;
+		else return true;
+	}
+	
+	
+	
+	/**
+	 * metoda odigraj(Koordinati poteza) vrne logièno vrednost 
+	 * spremeni vrednost igralca na potezi ter ga nastavi na nasprotnika
+	 * @param poteza
+	 * @return true, èe je polje prazno, false èe ni
+	 */
+	public boolean odigraj(Koordinati poteza) {
+		if (plosca[poteza.getX()][poteza.getY()] == Polje.PRAZNO) {
+			plosca[poteza.getX()][poteza.getY()] = naPotezi.getPolje();
+			naPotezi = naPotezi.nasprotnik();
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 	
 	
 }
